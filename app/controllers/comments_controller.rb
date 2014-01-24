9cac1bbd21c0c5b0e211d489f4445119403bbcea
Alex Meyers <ajm339@cornell.edu>
2014-01-24 04:20:15 -0500

    fix comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,9 @@ class CommentsController < ApplicationController
     
     def create
         @post = Post.find(params[:post_id])
-        #@comment = @post.comments.create!(comment_params)
-        puts '================================'
-        puts comment_params
-        puts params[:body].nil?.inspect
-        puts params[:name].nil?.inspect
-        puts params[:email].nil?.inspect
-
+        
         respond_to do |format|
-	      if (comment_params[:body].nil? || comment_params[:name].nil? || comment_params[:email].nil?)
+	      if (comment_params[:body].present? && comment_params[:name].present? && comment_params[:email].present?)
 	      	@comment = @post.comments.create!(comment_params)
 	        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
 	        format.json { render action: 'show', status: :created, location: @post }
